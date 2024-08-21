ae16abcb02b152a71f133159a59de0c6d7d24d93
Jayraj Chauhan <jchauhan@buchanan.com>
2024-08-21 21:02:10 +0530

    adding tf code

---
+++ 6.ecs_service.tf
@@ -0,0 +1,32 @@
+resource "aws_ecs_service" "aws-ecs-service" {
+  name                 = "${var.app_name}-${var.app_environment}-ecs-service"
+  cluster              = aws_ecs_cluster.aws-ecs-cluster.id
+  task_definition      = "${aws_ecs_task_definition.aws-ecs-task.arn}"
+  launch_type          = "FARGATE"
+  scheduling_strategy  = "REPLICA"
+  desired_count        = 2
+  force_new_deployment = true
+
+  network_configuration {
+    subnets          = aws_subnet.public.*.id
+    assign_public_ip = true
+    security_groups = [
+      aws_security_group.service_security_group.id,
+   #   aws_security_group.load_balancer_security_group.id
+    ]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.target_group.arn
+    container_name   = "${var.app_name}-${var.app_environment}-container"
+    container_port   = 8080
+  }
+  depends_on = [aws_lb_listener.listener]
+
+   tags = {
+    Name        = "${var.app_name}-ecs-service"
+    Environment = var.app_environment
+   }
+
+
+}
